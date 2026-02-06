35366cf329a47ab38022127e73569af7cbaf7831
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2026-02-06 23:43:05 +0300

    h4-5

---
+++ src/components/SingIn.jsx
@@ -0,0 +1,8 @@
+
+import AuthForm from "./auth/AuthForm.jsx";
+
+const SignIn = ({setIsAuth}) => {
+    return <AuthForm isSignUp={false} setIsAuth={setIsAuth}/>;
+};
+
+export default SignIn;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,6 @@
+import SignIn from "../components/SingIn.jsx";
+
+const SignInPage = ({setIsAuth}) => {
+    return <SignIn setIsAuth={setIsAuth}/>;
+};
+export default SignInPage;
